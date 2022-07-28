e0e8da89f86ac8642aed923004fe584f46e113e8
koreacat <kj9966@naver.com>
2022-07-28 15:51:31 +0900

    level2

--- js/src/programmers/level2/프린터.js
+++ js/src/programmers/level2/프린터.js
@@ -1,17 +1,14 @@
-const sort = (arr, sortedArr) => {
-    if(arr.length === 0) return;
-    if(arr[0].value >= Math.max(...arr.map(d => d.value))) sortedArr.push(arr.shift());
-    else arr.push(arr.shift());
-    sort(arr, sortedArr);
-}
-
 function solution(priorities, location) {
+    const arr = priorities.map((v, i) => { return {index: i, value: v}});
     const sortedArr = [];
-    sort(priorities.map((v, i) => { return {index: i, value: v}}), sortedArr);
+
+    while(arr.length) {
+        if(arr[0].value >= Math.max(...arr.map(d => d.value))) sortedArr.push(arr.shift());
+        else arr.push(arr.shift());
+    }
+
     return sortedArr.findIndex(e => e.index === location) + 1;
 }
 
 solution([2, 1, 3, 2], 2);
 solution([1, 1, 9, 1, 1, 1], 0);
-
-// 재귀 말고 while 문으로 하면 더 빠를듯함
